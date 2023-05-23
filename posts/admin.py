19d8eea9732962ee37a6361cca81b55de94985fb
noemichis <noemi_chis@yahoo.com>
2023-05-23 17:47:31 +0000

    Add and register Categories and Comments to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Categories, Comment
 from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Add Categories fields to admin panel
+    """
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """
+    Add Post fields to admin panel with summernote field for content
+    """
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Add Comments field to admin panel
+    """
+    actions = ['approve_comment']
+
+    def approve_comment(self, request, queryset):
+        queryset.update(approved=True)
