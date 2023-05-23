1570bf391dba5ca02bcf9f10d87567ad740c10d5
noemichis <noemi_chis@yahoo.com>
2023-05-23 18:05:42 +0000

    Add display/filter functionality to admin panel items

--- posts/admin.py
+++ posts/admin.py
@@ -8,13 +8,19 @@ class CategoryAdmin(admin.ModelAdmin):
     """
     Add Categories fields to admin panel
     """
+    list_display = ['category_name']
+    search_fields = ['category_name']
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """
-    Add Post fields to admin panel with summernote field for content
+    Add Post fields to admin panel with Summernote field for content
     """
+    list_display = ('title', 'slug', 'status', 'date_posted', 'category')
+    search_fields = ['title', 'content', 'category']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'date_posted')
     summernote_fields = ('content')
 
 
@@ -23,6 +29,9 @@ class CommentAdmin(admin.ModelAdmin):
     """
     Add Comments field to admin panel
     """
+    list_display = ('author', 'related_post', 'body', 'comment_on', 'approved')
+    search_fields = ['author', 'body']
+    list_filter = ('approved', 'comment_on')
     actions = ['approve_comment']
 
     def approve_comment(self, request, queryset):
