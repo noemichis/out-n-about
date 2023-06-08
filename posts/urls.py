ff7d76ff55deb7f910c874021d3b8fc0fd8bfdbe
noemichis <noemi_chis@yahoo.com>
2023-06-08 19:49:35 +0000

    Add path for  post_detail

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='home'),
     path('categories/<str:category_name>', views.categories_view, name='categories'),
+    path('post_detail/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
     # path('categories/', views.category_posts, name='categories')
 ]
