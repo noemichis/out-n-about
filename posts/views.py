08c94a616fedbb99aec32379af4ff023c79be4b1
noemichis <noemi_chis@yahoo.com>
2023-06-09 16:52:12 +0000

    Remove categories from views context

--- posts/views.py
+++ posts/views.py
@@ -4,10 +4,8 @@ from .models import Post, Categories
 
 
 def index(request):
-    get_cats = Categories.objects.all()
     get_posts = Post.objects.all()
     context = {
-        'categories_list': get_cats,
         'post_list': get_posts
     }
 
@@ -15,13 +13,11 @@ def index(request):
 
 
 def categories_view(request, category_name):
-    categories = Categories.objects.all()
     selected_category = Categories.objects.get(category_name=category_name)
     categories_post = Post.objects.filter(
         category=selected_category
         )
     context = {
-        'categories_list': categories,
         'selected_category': selected_category,
         'posts': categories_post
     }
