1e4c0b32472593540781d8cf7120c667f28bd442
noemichis <noemi_chis@yahoo.com>
2023-06-13 09:25:56 +0000

    AddPOST method for PostDetail view comments

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Post, Categories
+from .models import Post, Categories, Comment
 from .forms import CommentForm
 
 
@@ -27,12 +27,38 @@ def categories_view(request, category_name):
 
 
 class PostDetail(View):
-
+    """
+    Renders the post details Page
+    """
     def get(self, request, slug, *args, **kwargs):
-
         get_posts = Post.objects.all()
         post = get_object_or_404(get_posts, slug=slug)
         comments = post.comments.filter(approved=True)
+        context = {
+            'post': post,
+            'comments': comments,
+            'comment_form': CommentForm(),
+        }
+
+        return render(request, 'post_detail.html', context)
+
+    def post(self, request, slug, *args, **kwargs):
+        """
+        Allows commenting on the posts
+        """
+        get_posts = Post.objects.all()
+        post = get_object_or_404(get_posts, slug=slug)
+        comments = post.comments.filter(approved=True)
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.related_post = post
+            comment.author = request.user
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
         context = {
             'post': post,
             'comments': comments,
