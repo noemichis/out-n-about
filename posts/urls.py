5e20b80f2ec9388d131dd9dbc54a55c7f35e27b2
noemichis <noemi_chis@yahoo.com>
2023-07-01 22:41:28 +0000

    Add

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,19 @@
 from . import views
 from django.urls import path
 
+"""
+Posts URL paths
+"""
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
-    path('categories/<str:category_name>', views.categories_view, name='categories'),
-    path('post_detail/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
-    path('comment_delete/<int:id>', views.comment_delete, name='comment_delete'),
-    path('comment_edit/<int:pk>', views.CommentEdit.as_view(), name='comment_edit'),
+    path('categories/<str:category_name>', views.categories_view,
+         name='categories'),
+    path('post_detail/<slug:slug>', views.PostDetail.as_view(),
+         name='post_detail'),
+    path('comment_delete/<int:id>', views.comment_delete,
+         name='comment_delete'),
+    path('comment_edit/<int:pk>', views.CommentEdit.as_view(),
+         name='comment_edit'),
     path('about', views.about, name='about'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
